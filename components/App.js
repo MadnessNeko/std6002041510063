49a22c5b359600c7aab89986cfce102c89049d6a
สมเจตน์ วงษ์เหรียญทอง <s6002041510063@Macmini-213-15.local>
2019-03-28 15:22:54 +0700

    create Card,CardSec add on App

--- components/App.js
+++ components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Header from './Header';
+import Card from './Card';
 
 class App extends React.Component {
     render() {
         return (
             <View>
                 <Header title="Album"></Header>
+                <Card></Card>
                 <Text>Somjade Wongriantong</Text>
             </View>
        );
