e3fc94b9e988e700127bb7c31490c8b72ab9e22b
สมเจตน์ วงษ์เหรียญทอง <s6002041510063@Macmini-213-15.local>
2019-03-28 14:22:55 +0700

    add Header to App

--- components/App.js
+++ components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from './Header';
 
 class App extends React.Component {
     render() {
         return (
             <View>
+                <Header title="Album"></Header>
                 <Text>Somjade Wongriantong</Text>
             </View>
        );
