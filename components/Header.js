e9962a7c0ed4a6db0cdab821871cb259c47d5c40
สมเจตน์ วงษ์เหรียญทอง <s6002041510063@Macmini-213-15.local>
2019-03-28 14:53:52 +0700

    edit Header

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,22 @@ import { View, Text } from 'react-native';
 class Header extends Component{
     render() {
         return (
-            <View>
-                <Text>{this.props.title}</Text>
+            <View style= { styles.header }>
+                <Text style= { styles.text }>{this.props.title}</Text>
             </View>
         );
+    }
+}
+
+const styles = {
+    header: {
+        padding: 20,
+        alignItems: 'center',
+        backgroundColor: '#BEBEBE'
+    },
+    text: {
+        fontSize: 30,
+        color: '#FE0000'
     }
 }
 
